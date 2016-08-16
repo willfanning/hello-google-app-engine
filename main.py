3598238035ae694390a35ab32a12e163c87d2849
Will <william.fanning1@gmail.com>
2016-08-16 16:17:27 -0500

    completed udacity-253 pSet2 ROT13

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import cgi
+
+form = """
+    <h2>Enter some text to ROT13:</h2>
+    <form action="/" method="POST">
+        <textarea name="text" style="height: 100px; width: 400px;">%(user_input)s</textarea>
+        <br>
+        <input type="submit">
+    </form>
+    """
+
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+    def write_form(self, text=''):
+        self.response.write(form % {'user_input':text})
+
+    def get(self):
+        #print(self.request)
+        self.response.write(form % {'user_input':''})
+
+    def post(self):
+        user_input = rot13(self.request.get('text'))
+        self.write_form(text=user_input)
+
+
+app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
+
+
+def rot13(user_input):
+    alpha = {'a': 'n', 'b': 'o', 'c': 'p', 'd': 'q', 'e': 'r', 'f': 's', 'g': 't', 'h': 'u', 'i': 'v', 'j': 'w',
+             'k': 'x',
+             'l': 'y', 'm': 'z', 'n': 'a', 'o': 'b', 'p': 'c', 'q': 'd', 'r': 'e', 's': 'f', 't': 'g', 'u': 'h',
+             'v': 'i',
+             'w': 'j', 'x': 'k', 'y': 'l', 'z': 'm'}
+    resp = ""
+    for s in user_input:
+        if s.isupper() and s.lower() in alpha:
+            resp += alpha.get(s.lower()).upper()
+        elif s in alpha:
+            resp += alpha.get(s)
+        else:
+            resp += s
+    return cgi.escape(resp)
+
